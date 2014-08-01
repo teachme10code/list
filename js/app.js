543d1cb8c2fe0dafecc846de2c5d5e8079055de6
Chris Ward <chris.ward@servicenow.com>
2014-07-31 21:01:26 -0700

    Made header color change on click +Minor CSS changes

--- js/app.js
+++ js/app.js
@@ -3,3 +3,17 @@ shopping list app
 chris ward
 7/27/14
 */
+
+//think about making this a javascript function and calling it on click from html
+/*$(document).ready(function() {*/
+var color = 0;
+function changeHeaderColor() {
+	var colors = ["#FF4081", "#3F51B5", "#0288d1", "#607d8b" ,"#009688"];
+	if (color === colors.length) {
+		color = 0;
+	}
+	console.log(colors[color]);
+	$('.list-header').css("background-color", colors[color])
+	color++
+};
+/*});*/
