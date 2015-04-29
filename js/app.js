0f9d61ebe88080decaf4d55385f30a2e3a8b3121
teachme10code <thinkfulchris@gmail.com>
2015-04-28 23:06:16 -0700

    ...and here's app.js

---
+++ js/app.js
@@ -0,0 +1,49 @@
+(function() {
+  var app = angular.module('proCon', []);
+
+  app.controller('ListController', function(){
+    this.products = gems;
+  });
+
+  app.controller('AddItemController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+    app.controller('ResultsController', function(){
+    this.products = gems;
+  });
+
+  var gems = [{
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        "images/gem-02.gif",
+        "images/gem-05.gif",
+        "images/gem-09.gif"
+      ],
+      reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org",
+        createdOn: 1397490980837
+      }]
+    }];
+})();
